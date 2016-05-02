5171df78bdc90f36db163726a82a3722b0d06fdb
wqh <wqh@wqhdeMacBook-Pro.local>
2016-05-02 16:57:57 +0800

    初始化项目

---
+++ YM/YM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YM
+//
+//  Created by wqh on 16/5/2.
+//  Copyright © 2016年 wqh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YM/YM/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YM
+//
+//  Created by wqh on 16/5/2.
+//  Copyright © 2016年 wqh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
